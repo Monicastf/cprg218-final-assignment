8cb1952b068dec42461750a5c739312eeefc892a
Monicastf <monicas.f555@gmail.com>
2024-12-12 00:03:31 -0700

    changes

---
+++ menu.js
@@ -0,0 +1,9 @@
+
+const backToTopButton = document.getElementById('back-to-top');
+
+backToTopButton.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
